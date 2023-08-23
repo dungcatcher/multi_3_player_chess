bf9c4a663573c876e91d73309eb03f2b74d4ed21
ewano <ewan.odenthal1@gmail.com>
2023-08-23 13:10:41 +1000

    clock stuff

--- client/States/Game/clock.py
+++ client/States/Game/clock.py
@@ -1,7 +1,15 @@
 from ...widget import Label
+import time
 
 
 class Clock:
-    def __init__(self, rect):
+    def __init__(self, rect, start_time):
         self.rect = rect
+        self.time = start_time
+        self.is_ticking = False
+
+    def update(self):
+        if self.is_ticking:
+            pass
+
 
